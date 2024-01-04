b33c21412f775f99816acfc73bd67836cfc955dc
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2024-01-04 18:01:00 +0100

    configure tailwind for react app

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,9 @@
+
+export default function Card() {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/1126975/pexels-photo-1126975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></img>
+      <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1>
+    </div>
+  )
+}
