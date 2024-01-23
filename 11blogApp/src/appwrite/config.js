252942db4eb0a4954c5a1ccfb34f5413eede39e2
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2024-01-23 12:12:42 +0100

    Handle CRUD configuration

--- 11blogApp/src/appwrite/config.js
+++ 11blogApp/src/appwrite/config.js
@@ -11,16 +11,68 @@ export class Service {
     this.databases = new Databases(this.client)
     this.bucket = new Storage(this.client)
   }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
+    } catch (error) {
+      console.log("Appwrite Service :: getPost() ::", error)
+      false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries)
+    } catch (error) {
+      console.log("Appwrite Service :: getPosts() ::", error)
+      false;
+    }
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId}) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title, content, featuredImage, status, userId
+        }
+      )
+    } catch (error) {
+      console.log("Appwrite Service :: createPost() ::", error)
+      false;
+    }
+  }
+
+  async updatePost(slug, {title, content, featuredImage, status}) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title, content, featuredImage, status, userId
+        }
+      )
+    } catch (error) {
+      console.log("Appwrite Service :: updateDocument() ::", error)
+      false;
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+        )
+      return true;
+    } catch (error) {
+      console.log("Appwrite Service :: updateDocument() ::", error)
+      false;
+    }
+  }
 }
-
-
-import { Client, Databases, Storage, Query } from "appwrite";
-
-const client = new Client();
-
-const databases = new Databases(client);
-
-client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('5df5acd0d48c2') // Your project ID
-;
